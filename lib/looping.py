f6b35498bea256481c87666fdb56d0afdfa0c92b
LeightonSkervin <lskervin23@gmail.com>
2024-03-11 19:15:02 -0400

    lab complte

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    if i == 0:
+        print('Happy New Year!')
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_integers = []
+
+    for element in int_list:
+        square_integers += [(element * element)]
+    return(square_integers)
 
 def fizzbuzz():
     # code goes here!
+    x = 1
+    while x < 101:
+        if x % 3 == 0 and x % 5 == 0:
+            print('FizzBuzz')
+        elif x % 3 == 0:
+            print('Fizz')
+        elif x % 5 ==0:
+            print('Buzz')
+        else:
+            print(x)
+        x += 1
     pass
